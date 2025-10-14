f528e5bf869f9806de7e23fc849d1ad5652ec575
Pedro Bueno <pbueno159@gmail.com>
2025-10-13 18:11:52 -0600

    Add routing and basic page structure with Home and Calendar components
    - Integrated react-router-dom for navigation
    - Created Home and Calendar components
    - Updated Header for login/logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Calendar from "./pages/Calendar";
 
 export default function App() {
+  localStorage.setItem("loginStatus", JSON.stringify(false));
+
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Calendar />} />
+          <Route path="/login" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,36 @@
 export default function Header() {
+  function handleLoginStatus(e) {
+    e.preventDefault();
+    localStorage.setItem(
+      "loginStatus",
+      JSON.stringify(!JSON.parse(localStorage.getItem("loginStatus")))
+    );
+  }
+
   return (
     <header>
-      <h1>capycal3.0</h1>
+      <nav>
+        <ul className="flex gap-4">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/home">Calendar</a>
+          </li>
+          <li>
+            <a
+              href="/login"
+              onClick={(e) => {
+                handleLoginStatus(e);
+              }}
+            >
+              {JSON.parse(localStorage.getItem("loginStatus"))
+                ? "Logout"
+                : "Login"}
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
