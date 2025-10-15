19e6e2245257205f739716490a8dbd3f3405014d
Pedro Bueno <pbueno159@gmail.com>
2025-10-15 13:00:54 -0600

    Revamp Home component with detailed description and features of Capycal app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Home</h1>
+      <h1 className="text-3xl font-bold underline text-center">Capycal</h1>
+      <h2>Welcome to Capycal!</h2>
+      <p>
+        Capycal is a calendar app that allows you to create and manage your own
+        events.
+      </p>
+      <p>
+        It's a calendar application themed after Capybaras. These cute and funny
+        creatures are the main characters of the app. Around every corner you'll
+        find a Capybara having a good time! While keeping you on task and ready
+        for your day.
+      </p>
+
+      <h3>Features:</h3>
+      <ul>
+        <li>Calendar</li>
+        <li>Event creation</li>
+        <li>Event editing</li>
+        <li>Event deletion</li>
+        <li>Event reminders</li>
+        <li>Event notifications</li>
+      </ul>
+
+      <h4>Try it out, For free!</h4>
+
+      <p className="italic text-gray-500">
+        Capycal is a passion project created for my loving partner, a Capybara
+        enthusiast.
+      </p>
     </div>
   );
 }
